74997dbb731635ab6aa99aeaa52876ca2a8527a7
Max Healy <maxhealy01@gmail.com>
2020-12-29 20:41:12 -0600

    Added password hashing

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -49,10 +49,37 @@ router.post('/', (req, res) => {
   })
 })
 
+router.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    //res.json({ user: dbUserData });
+
+    // Verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if(!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  })
+
+})
+
 // PUT
 router.put('/:id', (req, res) => {
   // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
